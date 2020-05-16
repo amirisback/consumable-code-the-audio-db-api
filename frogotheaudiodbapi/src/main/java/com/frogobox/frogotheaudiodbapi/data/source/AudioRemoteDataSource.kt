8aa6741c98f581e686fcb0fd391460eef6cfc534
amirisback <faisalamircs@gmail.com>
2020-05-16 21:39:52 +0700

    add: Kick Start File Project

---
+++ frogotheaudiodbapi/src/main/java/com/frogobox/frogotheaudiodbapi/data/source/AudioRemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.frogobox.frogotheaudiodbapi.data.source
+
+/*
+ * Created by Faisal Amir on 16/05/2020
+ * consumable-code-the-audio-db-api Source Code
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2020 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+ 
+ 
